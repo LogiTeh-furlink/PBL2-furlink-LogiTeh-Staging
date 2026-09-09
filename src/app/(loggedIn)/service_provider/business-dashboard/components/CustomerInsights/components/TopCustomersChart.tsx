cc7f625636bebbba217d8a141f901868b8a66137
Atasha Frances Gayle Doria <asdoria@student.apc.edu.ph>
2026-09-09 14:37:16 +0000

    fix bug

--- src/app/(loggedIn)/service_provider/business-dashboard/components/CustomerInsights/components/TopCustomersChart.tsx
+++ src/app/(loggedIn)/service_provider/business-dashboard/components/CustomerInsights/components/TopCustomersChart.tsx
@@ -60,8 +60,8 @@ export default function TopCustomersChart({ customers }: TopCustomersChartProps)
         grid: { display: false },
         ticks: {
           font: { size: 11 },
-          callback: function(val: string | number): string {
-            const label = customers[val as number]?.name || '';
+          callback: function(val: any): string {
+            const label = customers[val]?.name || '';
             return label.length > 15 ? label.substring(0, 15) + '...' : label;
           }
         },
